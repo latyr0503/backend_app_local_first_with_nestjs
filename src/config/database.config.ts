251bfb479bdc8ce6c2eba5b387db7b23830a43f0
Abdoulaye Latyr SENE <latyr0503@gmail.com>
2025-08-28 00:34:08 +0000

    correction config

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -5,17 +5,16 @@ export const getDatabaseConfig = (
   configService: ConfigService,
 ): TypeOrmModuleOptions => ({
   type: 'postgres',
-  host: configService.get('DB_HOST', 'localhost'),
-  port: configService.get('DB_PORT', 5432),
-  username: configService.get('DB_USERNAME', 'Admin_latyr'),
-  password: configService.get('DB_PASSWORD', 'Passer@123'),
-  database: configService.get('DB_DATABASE', 'watermelondb'),
+  host: configService.get('DB_HOST'),
+  port: configService.get('DB_PORT'),
+  username: configService.get('DB_USERNAME'),
+  password: configService.get('DB_PASSWORD'),
+  database: configService.get('DB_DATABASE'),
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: configService.get('NODE_ENV') !== 'production',
   logging: configService.get('NODE_ENV') !== 'production',
-  ssl:
-    configService.get('NODE_ENV') === 'production'
-      ? { rejectUnauthorized: false }
-      : false,
+  ssl: {
+    rejectUnauthorized: false,
+  },
   autoLoadEntities: true,
 });
